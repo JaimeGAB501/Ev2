cf8e9c3f4057833cdef031404c50f136df56e071
JaimeGAB501 <jai.gajardob@duocuc.cl>
2024-11-06 03:11:04 -0300

    Se modifico servicio para iniciar sesion

--- src/app/sevicio/auth/auth.service.ts
+++ src/app/sevicio/auth/auth.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { CuerpoLogin} from './../../interfaces/CuerpoLogin';
+import { UsuarioLogeado} from './../../interfaces/UsuarioLogeado'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly URL_LOGIN: string = 'https://dummyjson.com/auth/login';
+  public usuarioLogeado: UsuarioLogeado | null = null;
+  public accesToken: string | null = null;
+  constructor(
+    private http: HttpClient
+  ) {
 
-  constructor() { }
-}
+   }
+
+   public iniciarSesion(nombre_usuario: string, contrasenia: string){
+    const cuerpo: CuerpoLogin ={
+      username: nombre_usuario,
+      password: contrasenia
+    }
+    this.http.post<UsuarioLogeado>(this.URL_LOGIN, JSON.stringify(cuerpo),{
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .subscribe(resultado => {
+      this.usuarioLogeado = resultado;
+      this.accesToken = resultado.acessToken;
+      console.log(resultado);
+    });
+  }
+   }
+
