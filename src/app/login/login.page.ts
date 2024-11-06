09c81613c3450da3ca5dcf93b3126e1dcd351b18
JaimeGAB501 <jai.gajardob@duocuc.cl>
2024-11-06 04:41:26 -0300

    Pagina Simple de Login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { ViewWillEnter, ViewDidLeave } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { AuthService } from './../sevicio/auth/auth.service';
+import {Subscription} from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements ViewWillEnter, ViewDidLeave {
+  public formulario: FormGroup;
+  public cargando_bloqueo: boolean=false;
+  public subCargando!: Subscription;
+  constructor(
+    private fb : FormBuilder,
+    private auth: AuthService
+  ) { 
+    this.formulario = fb.group({
+      usuario: ['', [Validators.required]],
+      contrasenia: ['', [Validators.required]]
+    }
 
-  constructor() { }
+    )
 
-  ngOnInit() {
+  }
+  public validarFormulario(){
+    const esValido = this.formulario.valid;
+    if(esValido){
+      return  
+    }
+    const datos = this.formulario.getRawValue()
+    const usuario = datos['usuario'];
+    const contrasenia = datos['contrasenia'];
+    this.auth.iniciarSesion(usuario, contrasenia)
+
+  }
+
+  public ionViewWillEnter(): void {
+    this.subCargando = this.auth.cargando.subscribe(nuevoValor => {
+      this.cargando_bloqueo = nuevoValor;
+    })
+    
+  }
+  public ionViewDidLeave(): void {
+    if(this.subCargando){
+      this.subCargando.unsubscribe();
+    }
+    
   }
 
 }
--- src/app/sevicio/auth/auth.service.ts
+++ src/app/sevicio/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { CuerpoLogin} from './../../interfaces/CuerpoLogin';
 import { UsuarioLogeado} from './../../interfaces/UsuarioLogeado'; 
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,9 @@ export class AuthService {
   private readonly URL_LOGIN: string = 'https://dummyjson.com/auth/login';
   public usuarioLogeado: UsuarioLogeado | null = null;
   public accesToken: string | null = null;
+
+  private $cargando = new BehaviorSubject<boolean>(false);
+  public cargando = this.$cargando.asObservable();
   constructor(
     private http: HttpClient
   ) {
@@ -17,6 +21,8 @@ export class AuthService {
    }
 
    public iniciarSesion(nombre_usuario: string, contrasenia: string){
+    this.$cargando.next(true);
+
     const cuerpo: CuerpoLogin ={
       username: nombre_usuario,
       password: contrasenia
@@ -29,8 +35,15 @@ export class AuthService {
     .subscribe(resultado => {
       this.usuarioLogeado = resultado;
       this.accesToken = resultado.acessToken;
+      this.$cargando.next(false);
       console.log(resultado);
     });
+  }
+  public cerrarSesion(){
+    if(this.usuarioLogeado){
+    this.usuarioLogeado = null;
+    this.accesToken = null;
+    }
   }
    }
 
